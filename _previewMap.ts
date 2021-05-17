d11de2c96a73bc453e5080e4eaff178007fc7307
Igor Kostyakov <i.a.kostyakov@gmail.com>
2021-05-18 00:05:10 +0300

    clean up code in _previewMap script

--- _previewMap.ts
+++ _previewMap.ts
@@ -1,15 +1,18 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path');
-const { map, mapSize } = require(path.resolve(__dirname, process.argv[2]));
+import path from 'path';
+import { POIIcon } from '@locations/AreaMap';
+import { Size } from '@utils/@types';
+
+// eslint-disable-next-line import/no-dynamic-require, global-require, @typescript-eslint/no-var-requires
+const { map, mapSize } = require(path.resolve(__dirname, process.argv[2])) as { map: POIIcon[], mapSize: Size };
 
 const previewMap = () => {
   let result = '';
-  for (let y = 0; y < mapSize.height; y++) {
-    for (let x = 0; x < mapSize.width; x++) {
+  for (let y = 0; y < mapSize.height; y += 1) {
+    for (let x = 0; x < mapSize.width; x += 1) {
       result += map[y * mapSize.width + x];
       result += ' ';
     }
-    result = result.trim() + '\n';
+    result = `${result.trim()}\n`;
   }
   console.log(result);
 };
